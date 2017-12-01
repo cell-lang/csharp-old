fedc287f2fddf22069339054b303ca76ac810e14
cell-lang <cell.lang.dev@gmail.com>
2017-12-01 17:12:32 +0100

    IMPLEMENTED BASIC OBJECT HIERARCHY

---
+++ src/csharp/objs.cs
@@ -0,0 +1,752 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+
+namespace CellLang {
+  public abstract class Obj {
+    public virtual bool IsBlankObj()                                  {return false;}
+    public virtual bool IsNullObj()                                   {return false;}
+    public virtual bool IsSymb()                                      {return false;}
+    // public virtual bool IsBool()                                      {return false;}
+    public virtual bool IsInt()                                       {return false;}
+    public virtual bool IsFloat()                                     {return false;}
+    public virtual bool IsSeq()                                       {return false;}
+    public virtual bool IsEmptySeq()                                  {return false;}
+    public virtual bool IsNeSeq()                                     {return false;}
+    public virtual bool IsEmptyRel()                                  {return false;}
+    public virtual bool IsSet()                                       {return false;}
+    public virtual bool IsNeSet()                                     {return false;}
+    public virtual bool IsBinRel()                                    {return false;}
+    public virtual bool IsNeBinRel()                                  {return false;}
+    public virtual bool IsNeMap()                                     {return false;}
+    public virtual bool IsTernRel()                                   {return false;}
+    public virtual bool IsNeTernRel()                                 {return false;}
+    public virtual bool IsTagged()                                    {return false;}
+
+    public virtual bool IsSymb(int id)                                {return false;}
+    public virtual bool IsInt(long n)                                 {return false;}
+    public virtual bool IsFloat(double x)                             {return false;}
+
+    public virtual bool HasElem(Obj o)                                {throw new Exception();}
+    public virtual bool HasKey(Obj o)                                 {throw new Exception();}
+    public virtual bool HasField(int id)                              {throw new Exception();}
+    public virtual bool HasPair(Obj o1, Obj o2)                       {throw new Exception();}
+    public virtual bool HasTriple(Obj o1, Obj o2, Obj o3)             {throw new Exception();}
+
+    public virtual int    GetSymbId()                                 {throw new Exception();}
+    public virtual bool   GetBool()                                   {throw new Exception();}
+    public virtual long   GetInt()                                    {throw new Exception();}
+    public virtual double GetFloat()                                  {throw new Exception();}
+    public virtual int    GetSize()                                   {throw new Exception();}
+    public virtual Obj    GetItem(long i)                             {throw new Exception();}
+    public virtual int    GetTagId()                                  {throw new Exception();}
+    public virtual Obj    GetInnerObj()                               {throw new Exception();}
+
+    public virtual Obj Lookup(Obj key)                                {throw new Exception();}
+    public virtual Obj LookupField(int id)                            {throw new Exception();}
+
+    public virtual bool IsEq(Obj o) {
+      return Cmp(o) == 0;
+    }
+
+    public int Cmp(Obj o) {
+      int id1 = TypeId();
+      int id2 = o.TypeId();
+      if (id1 == id2)
+        return InternalCmp(o);
+      return id1 < id2 ? 1 : -1;
+    }
+
+    public virtual int CmpSeq(Obj[] es, int o, int l)              {throw new Exception();}
+    public virtual int CmpNeSet(Obj[] es)                          {throw new Exception();}
+    public virtual int CmpNeBinRel(Obj[] c1, Obj[] c2)             {throw new Exception();}
+    public virtual int CmpNeTernRel(Obj[] c1, Obj[] c2, Obj[] c3)  {throw new Exception();}
+    public virtual int CmpTaggedObj(int tag, Obj obj)              {throw new Exception();}
+
+    protected abstract int TypeId();
+    protected abstract int InternalCmp(Obj o);
+  }
+
+
+  class SymbObj : Obj {
+    int id;
+
+    SymbObj(int id) {
+      this.id = id;
+    }
+
+    override public bool IsSymb() {
+      return true;
+    }
+
+    override public bool IsSymb(int id) {
+      return this.id == id;
+    }
+
+    override public int GetSymbId() {
+      return id;
+    }
+
+    override public bool GetBool() {
+      if (id == 0)
+        return false;
+      if (id == 1)
+        return true;
+      throw new Exception();
+    }
+
+    override public bool IsEq(Obj obj) {
+      return obj.IsSymb(id);
+    }
+
+    override protected int TypeId() {
+      return 0;
+    }
+
+    override protected int InternalCmp(Obj other) {
+      int other_id = other.GetSymbId();
+      return id == other_id ? 0 : (id < other_id ? 1 : -1);
+    }
+  }
+
+
+  class IntObj : Obj {
+    long value;
+
+    IntObj(long value) {
+      this.value = value;
+    }
+
+    override public bool IsInt() {
+      return true;
+    }
+
+    override public bool IsInt(long value) {
+      return this.value == value;
+    }
+
+    override public long GetInt() {
+      return value;
+    }
+
+    override public bool IsEq(Obj obj) {
+      return obj.IsInt(value);
+    }
+
+    override protected int TypeId() {
+      return 1;
+    }
+
+    override protected int InternalCmp(Obj obj) {
+      long other_value = obj.GetInt();
+      return value == other_value ? 0 : (value < other_value ? 1 : -1);
+    }
+  }
+
+
+  class FloatObj : Obj {
+    double value;
+
+    FloatObj(double value) {
+      this.value = value;
+    }
+
+    override public bool IsFloat() {
+      return true;
+    }
+
+    override public bool IsFloat(double value) {
+      return this.value == value;
+    }
+
+    override public double GetFloat() {
+      return value;
+    }
+
+    override public bool IsEq(Obj obj) {
+      return obj.IsFloat(value);
+    }
+
+    override protected int TypeId() {
+      return 2;
+    }
+
+    override protected int InternalCmp(Obj other) {
+      double other_value = other.GetFloat();
+      return value == other_value ? 0 : (value < other_value ? 1 : -1);
+    }
+  }
+
+
+  class SeqObj : Obj {
+    int length;
+    int used;
+    Obj[] items;
+
+    SeqObj(Obj[] items, int length) {
+      this.length = length;
+      this.used = length;
+      this.items = items;
+    }
+
+    override public bool IsSeq() {
+      return true;
+    }
+
+    override public bool IsEmptySeq() {
+      return length == 0;
+    }
+
+    override public bool IsNeSeq() {
+      return length != 0;
+    }
+
+    override public int GetSize() {
+      return length;
+    }
+
+    override public Obj GetItem(long idx) {
+      if (idx < length)
+        return items[idx];
+      else
+        throw new Exception();
+    }
+
+    override protected int TypeId() {
+      return 3;
+    }
+
+    override protected int InternalCmp(Obj other) {
+      return other.CmpSeq(items, 0, length);
+    }
+
+    override public int CmpSeq(Obj[] other_items, int other_offset, int other_length) {
+      if (other_length != length)
+        return other_length < length ? 1 : -1;
+      for (int i=0 ; i < length ; i++) {
+        int res = other_items[other_offset+i].Cmp(items[i]);
+        if (res != 0)
+          return res;
+      }
+      return 0;
+    }
+  }
+
+
+  class SliceObj : Obj {
+    int offset;
+    int length;
+    Obj[] items;
+
+    SliceObj(Obj[] items, int offset, int length) {
+      this.offset = offset;
+      this.length = length;
+      this.items = items;
+    }
+
+    override public bool IsSeq() {
+      return true;
+    }
+
+    override public bool IsEmptySeq() {
+      return length == 0;
+    }
+
+    override public bool IsNeSeq() {
+      return length != 0;
+    }
+
+    override public int GetSize() {
+      return length;
+    }
+
+    override public Obj GetItem(long idx) {
+      if (idx < length)
+        return items[offset+idx];
+      else
+        throw new Exception();
+    }
+
+    override protected int TypeId() {
+      return 3;
+    }
+
+    override protected int InternalCmp(Obj other) {
+      return other.CmpSeq(items, offset, length);
+    }
+
+    override public int CmpSeq(Obj[] other_items, int other_offset, int other_length) {
+      if (other_length != length)
+        return other_length < length ? 1 : -1;
+      for (int i=0 ; i < length ; i++) {
+        int res = other_items[other_offset+i].Cmp(items[offset+i]);
+        if (res != 0)
+          return res;
+      }
+      return 0;
+    }
+  }
+
+
+  class EmptyRel : Obj {
+    EmptyRel() {
+
+    }
+
+    override public bool IsEmptyRel() {
+      return true;
+    }
+
+    override public bool IsSet() {
+      return true;
+    }
+
+    override public bool IsBinRel() {
+      return true;
+    }
+
+    override public bool IsTernRel() {
+      return true;
+    }
+
+    override public bool IsEq(Obj obj) {
+      return obj.IsEmptyRel();
+    }
+
+    override public bool HasElem(Obj obj) {
+      return false;
+    }
+
+    override public bool HasKey(Obj key) {
+      return false;
+    }
+
+    override public bool HasField(int id) {
+      return false;
+    }
+
+    override public bool HasPair(Obj obj1, Obj obj2) {
+      return false;
+    }
+
+    override public bool HasTriple(Obj obj1, Obj obj2, Obj obj3) {
+      return false;
+    }
+
+    override public int GetSize() {
+      return 0;
+    }
+
+    override protected int TypeId() {
+      return 4;
+    }
+
+    override protected int InternalCmp(Obj other) {
+      return 0;
+    }
+  }
+
+
+  class NeSetObj : Obj {
+    Obj[] elts;
+
+    NeSetObj(Obj[] elts) {
+      Debug.Assert(elts == null || elts.Length > 0);
+      this.elts = elts;
+    }
+
+    override public bool IsSet() {
+      return true;
+    }
+
+    override public bool IsNeSet() {
+      return true;
+    }
+
+    override public bool HasElem(Obj obj) {
+      return ObjUtils.BinSearch(elts, obj) != -1;
+    }
+
+    override public int GetSize() {
+      return elts.Length;
+    }
+
+    override protected int TypeId() {
+      return 5;
+    }
+
+    override protected int InternalCmp(Obj other) {
+      return other.CmpNeSet(elts);
+    }
+
+    override public int CmpNeSet(Obj[] other_elts) {
+      int len = elts.Length;
+      int other_len = other_elts.Length;
+      if (other_len != len)
+        return other_len < len ? 1 : -1;
+      for (int i=0 ; i < 0 ; i++) {
+        int res = other_elts[i].Cmp(elts[i]);
+        if (res != 0)
+          return res;
+      }
+      return 0;
+    }
+  }
+
+
+  class NeBinRelObj : Obj {
+    Obj[] col1;
+    Obj[] col2;
+    bool isMap;
+
+    NeBinRelObj(Obj[] col1, Obj[] col2, bool isMap) {
+      Debug.Assert(col1 != null && col2 != null);
+      Debug.Assert(col1.Length > 0);
+      Debug.Assert(col1.Length == col2.Length);
+      this.col1 = col1;
+      this.col2 = col2;
+      this.isMap = isMap;
+    }
+
+    override public bool IsBinRel() {
+      return true;
+    }
+
+    override public bool IsNeBinRel() {
+      return true;
+    }
+
+    override public bool IsNeMap() {
+      return isMap;
+    }
+
+    override public bool HasKey(Obj obj) {
+      Debug.Assert(isMap);
+      return ObjUtils.BinSearch(col1, obj) != -1;
+    }
+
+    override public bool HasField(int symb_id) {
+      int len = col1.Length;
+      for (int i=0 ; i < len ; i++)
+        if (col1[i].IsSymb(symb_id))
+          return true;
+      return false;
+    }
+
+    override public bool HasPair(Obj obj1, Obj obj2) {
+      if (isMap) {
+        int idx = ObjUtils.BinSearch(col1, obj1);
+        return idx != -1 && col2[idx].IsEq(obj2);
+      }
+      else {
+        int first;
+        int count = ObjUtils.BinSearchRange(col1, 0, col1.Length, obj1, out first);
+        if (count == 0)
+          return false;
+        int idx = ObjUtils.BinSearch(col2, first, count, obj2);
+        return idx != -1;
+      }
+    }
+
+    override public int GetSize() {
+      return col1.Length;
+    }
+
+    override public Obj Lookup(Obj key) {
+      int idx = ObjUtils.BinSearch(col1, key);
+      if (idx == -1)
+        throw new Exception();
+      if (!isMap)
+        if ((idx > 0 && col1[idx-1].IsEq(key)) || (idx+1 < col1.Length && col1[idx+1].IsEq(key)))
+          throw new Exception();
+      return col2[idx];
+    }
+
+    override public Obj LookupField(int symb_id) {
+      int len = col1.Length;
+      for (int i=0 ; i < len ; i++)
+        if (col1[i].IsSymb(symb_id))
+          return col2[i];
+      // We should never get here. The typechecker should prevent it.
+      throw new InvalidOperationException();
+    }
+
+    override protected int TypeId() {
+      return 6;
+    }
+
+    override protected int InternalCmp(Obj other) {
+      return other.CmpNeBinRel(col1, col2);
+    }
+
+    override public int CmpNeBinRel(Obj[] other_col_1, Obj[] other_col_2) {
+      int len = col1.Length;
+      int other_len = other_col_1.Length;
+      if (other_len != len)
+        return other_len < len ? 1 : -1;
+      for (int i=0 ; i < 0 ; i++) {
+        int res = other_col_1[i].Cmp(col1[i]);
+        if (res != 0)
+          return res;
+      }
+      for (int i=0 ; i < 0 ; i++) {
+        int res = other_col_2[i].Cmp(col2[i]);
+        if (res != 0)
+          return res;
+      }
+      return 0;
+    }
+  }
+
+
+  class NeTernRelObj : Obj {
+    Obj[] col1;
+    Obj[] col2;
+    Obj[] col3;
+
+    NeTernRelObj(Obj[] col1, Obj[] col2, Obj[] col3) {
+      Debug.Assert(col1 != null && col2 != null && col3 != null);
+      Debug.Assert(col1.Length == col2.Length && col1.Length == col3.Length);
+      Debug.Assert(col1.Length > 0);
+      this.col1 = col1;
+      this.col2 = col2;
+      this.col3 = col3;
+    }
+
+    override public bool IsTernRel() {
+      return true;
+    }
+
+    override public bool IsNeTernRel() {
+      return true;
+    }
+
+    override public bool HasTriple(Obj obj1, Obj obj2, Obj obj3) {
+      int first;
+
+      int count = ObjUtils.BinSearchRange(col1, 0, col1.Length, obj1, out first);
+      if (count == 0)
+        return false;
+
+      count = ObjUtils.BinSearchRange(col2, first, count, obj2, out first);
+      if (count == 0)
+        return false;
+
+      int idx = ObjUtils.BinSearch(col3, first, count, obj3);
+      return idx != -1;
+    }
+
+    override public int GetSize() {
+      return col1.Length;
+    }
+
+    override protected int TypeId() {
+      return 7;
+    }
+
+    override protected int InternalCmp(Obj other) {
+      return other.CmpNeTernRel(col1, col2, col3);
+    }
+
+    override public int CmpNeTernRel(Obj[] other_col_1, Obj[] other_col_2, Obj[] other_col_3) {
+      int len = col1.Length;
+      int other_len = other_col_1.Length;
+      if (other_len != len)
+        return other_len < len ? 1 : -1;
+      for (int i=0 ; i < 0 ; i++) {
+        int res = other_col_1[i].Cmp(col1[i]);
+        if (res != 0)
+          return res;
+      }
+      for (int i=0 ; i < 0 ; i++) {
+        int res = other_col_2[i].Cmp(col2[i]);
+        if (res != 0)
+          return res;
+      }
+      for (int i=0 ; i < 0 ; i++) {
+        int res = other_col_3[i].Cmp(col3[i]);
+        if (res != 0)
+          return res;
+      }
+      return 0;
+    }
+  }
+
+
+  class TaggedObj : Obj {
+    int tag;
+    Obj obj;
+
+    TaggedObj(int tag, Obj obj) {
+      this.tag = tag;
+      this.obj = obj;
+    }
+
+    override public bool IsTagged() {
+      return true;
+    }
+
+    override public int GetTagId() {
+      return tag;
+    }
+
+    override public Obj GetInnerObj() {
+      return obj;
+    }
+
+    override protected int TypeId() {
+      return 8;
+    }
+
+    override protected int InternalCmp(Obj other) {
+      return other.CmpTaggedObj(tag, obj);
+    }
+
+    override public int CmpTaggedObj(int other_tag, Obj other_obj) {
+      if (other_tag != tag)
+        return other_tag < tag ? 1 : -1;
+      else
+        return other_obj.Cmp(obj);
+    }
+  }
+
+
+  static class ObjUtils {
+    public static int BinSearch(Obj[] objs, Obj obj) {
+      return BinSearch(objs, 0, objs.Length, obj);
+    }
+
+    public static int BinSearch(Obj[] objs, int first, int count, Obj obj) {
+      int low = first;
+      int high = first + count - 1;
+
+      while (low <= high) {
+        int mid = (int) (((long) low + (long) high) / 2);
+        switch (objs[mid].Cmp(obj)) {
+          case -1:
+            // objs[mid] > obj
+            high = mid - 1;
+            break;
+
+          case 0:
+            return mid;
+
+          case 1:
+            // objs[mid] < obj
+            low = mid + 1;
+            break;
+        }
+      }
+
+      return -1;
+    }
+
+    public static int BinSearchRange(Obj[] objs, int offset, int length, Obj obj, out int first) {
+      int low = offset;
+      int high = offset + length - 1;
+      int lower_bound = low;
+      int upper_bound = high;
+
+      while (low <= high) {
+        int mid = (int) (((long) low + (long) high) / 2);
+        switch (objs[mid].Cmp(obj)) {
+          case -1:
+            // objs[mid] > obj
+            upper_bound = high = mid - 1;
+            break;
+
+          case 0:
+            if (mid == offset || !objs[mid-1].IsEq(obj)) {
+              first = mid;
+              low = lower_bound;
+              high = upper_bound;
+              goto Next;
+            }
+            else
+              high = mid - 1;
+            break;
+
+          case 1:
+            // objs[mid] < obj
+            lower_bound = low = mid + 1;
+            break;
+        }
+      }
+
+      first = -1; //## IS THIS NECESSARY?
+      return 0;
+
+    Next:
+      while (low <= high) {
+        int mid = (int) (((long) low + (long) high) / 2);
+        switch (objs[mid].Cmp(obj)) {
+          case -1:
+            // objs[mid] > obj
+            high = mid - 1;
+            break;
+
+          case 0:
+            if (mid == upper_bound || !objs[mid+1].IsEq(obj)) {
+              return mid - first + 1;
+            }
+            else
+              low = mid + 1;
+            break;
+
+          case 1:
+            // objs[mid] < obj
+            low = mid + 1;
+            break;
+        }
+      }
+
+      // We're not supposed to ever get here.
+      throw new InvalidOperationException();
+    }
+  }
+
+
+  public static class SymbTable {
+    static string[] defaultSymbols = {
+      "false",
+      "true",
+      "void",
+      "string",
+      "nothing",
+      "just",
+      "success",
+      "failure"
+    };
+
+    static List<String> symbTable = new List<String>();
+    static Dictionary<String, int> symbMap = new Dictionary<String, int>();
+
+    static SymbTable() {
+      int len = defaultSymbols.Length;
+      for (int i=0 ; i < len ; i++) {
+        string str = defaultSymbols[i];
+        symbTable.Add(str);
+        symbMap.Add(str, i);
+      }
+    }
+
+    public static int StrToIdx(string str) {
+      int idx;
+      if (symbMap.TryGetValue(str, out idx))
+        return idx;
+      int count = symbTable.Count;
+      if (count < 65535) {
+        idx = count;
+        symbTable.Add(str);
+        symbMap.Add(str, idx);
+        return idx;
+      }
+      throw new Exception();
+    }
+
+    public static string IdxToStr(int idx) {
+      return symbTable[idx];
+    }
+  }
+}
